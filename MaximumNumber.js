bcda594078a3d3b008d9e1b8e7a3ba037872ab23
sanzida <sanzida7651@gmail.com>
2025-08-20 14:08:11 +0600

    first commit

---
+++ MaximumNumber.js
@@ -0,0 +1,11 @@
+function findMaximumNumber(nums) {
+    let maxnum = nums[0];
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] > maxnum){
+            maxnum = nums[i];
+        }
+    }
+    return maxnum;
+}
+
+console.log(findMaximumNumber([5, 1, 9, 3]));
---
+++ Palindrome.js
@@ -0,0 +1,18 @@
+function isPalindrome(str) {
+  let j = str.length - 1;
+  for(let i = 0; i < str.length / 2; i++) {
+    if(str[i] != str[j]){
+        return false;
+    }
+    j--;
+  }
+    return true;
+
+}
+
+let str1 = "madam";
+let str2 = "hello";
+
+
+console.log(isPalindrome("madam")); 
+console.log(isPalindrome("hello")); 
